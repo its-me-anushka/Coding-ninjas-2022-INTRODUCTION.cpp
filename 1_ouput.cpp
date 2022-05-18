72cdac768c0cc49cdfda3ab8d481a1165b3e2ba8
Anushka mudgal <105125740+its-me-anushka@users.noreply.github.com>
2022-05-18 22:27:39 +0530

    Create 1_ouput.cpp

---
+++ 1_ouput.cpp
@@ -0,0 +1,28 @@
+//  What is the output 
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 15;
+    if(x <= 15) {
+        cout << "Inside if ";
+    } else if(x == 15) {
+        cout << "Inside else if ";
+    }
+    cout << x;
+}
+
+/* What is the output 
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 15;
+    if(x <= 15) {
+        cout << "Inside if ";
+    } else if(x == 15) {
+        cout << "Inside else if ";
+    }
+    cout << x;
+} */ 
+
